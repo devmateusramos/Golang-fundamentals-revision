1dbe178ed6ddfb87ffc4d28e52c383404507a856
devmateusramos <devmateusramos@gmail.com>
2022-06-29 20:33:26 -0300

    padrão multiplexador

---
+++ concorrencia/8-padrao-multiplexador/padrao.multiplexador.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal := multiplexar(escrever("Olá mundo canal1"), escrever("Olá mundo canal2"))
+	for i := 0; i < 15; i++ {
+		fmt.Println(<-canal)
+	}
+}
+
+func multiplexar(canalDeEntrada1, canalDeEntrada2 <-chan string) <-chan string {
+	canalDeSaida := make(chan string)
+	// Assim centralizamos a chamada de 2 canais em apenas um
+	go func() {
+		for {
+			select {
+			case mensagem := <-canalDeEntrada1:
+				canalDeSaida <- mensagem
+			case mensagem := <-canalDeEntrada2:
+				canalDeSaida <- mensagem
+
+			}
+		}
+	}()
+
+	return canalDeSaida
+}
+
+func escrever(texto string) <-chan string {
+	canal := make(chan string)
+
+	go func() {
+		for {
+			canal <- fmt.Sprintf("Valor recebido: %s", texto)
+			time.Sleep(time.Millisecond * 600)
+		}
+	}()
+
+	return canal
+}
