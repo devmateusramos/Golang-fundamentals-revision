776e72c77a6841a428e61f611f5917f30c0f1adf
devmateusramos <devmateusramos@gmail.com>
2022-07-03 17:58:46 -0300

    banco de dados configurado

--- api-devbook/main.go
+++ api-devbook/main.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
 	config.Carregar()
-	fmt.Println(config.Porta)
 
 	routes := router.Gerar()
+	fmt.Printf("Server is running on port %d", config.Porta)
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), routes))
 }
