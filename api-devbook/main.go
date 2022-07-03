3ba030aa6ec17297534f105aa5afaf6f9e582579
devmateusramos <devmateusramos@gmail.com>
2022-07-03 17:49:37 -0300

    dot env, model Usuario and config

--- api-devbook/main.go
+++ api-devbook/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api-devbook/src/config"
 	"api-devbook/src/router"
 	"fmt"
 	"log"
@@ -8,8 +9,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Rodando API")
+	config.Carregar()
+	fmt.Println(config.Porta)
 
 	routes := router.Gerar()
-	log.Fatalln(http.ListenAndServe(":5000", routes))
+	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), routes))
 }
